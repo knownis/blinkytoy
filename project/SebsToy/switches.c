6611b2ae25d8e84e028e83aff25a222dba25cf26
Sebastian Gonzalez <33067271+seb1gonzalez@users.noreply.github.com>
2018-04-11 11:26:09 -0600

    Update switches.c

--- project/SebsToy/switches.c
+++ project/SebsToy/switches.c
@@ -4,7 +4,7 @@
 #include "buzzer.h"
 #include "led.h"
 
-
+// empty comments are purposely deleted functions 
 char switch_state_down,switch2_state_down,switch3_state_down, switch_state_changed;
 static char 
 switch_update_interrupt_sense()
@@ -12,7 +12,7 @@ switch_update_interrupt_sense()
   char p2val = P2IN;
   /* update switch interrupt to detect changes from current buttons */
   P2IES |= (p2val & SWITCHES);	/* if switch up, sense down */
-  P2IES &= (p2val | ~SWITCHES);	/* if switch down, sense up */
+  //
   return p2val;
 }
 
@@ -21,7 +21,7 @@ switch_init()			/* setup switch */
 {  
   P2REN |= SWITCHES;		/* enables resistors for switches */
   P2IE = SWITCHES;		/* enable interrupts from switches */
-  P2OUT |= SWITCHES;		/* pull-ups for switches */
+ //
   P2DIR &= ~SWITCHES;		/* set switches' bits for input */
   switch_update_interrupt_sense();
   switch_interrupt_handler();	/* to initially read the switches */
@@ -35,27 +35,24 @@ switch_interrupt_handler()
   
   if(!(p2val & SW1)){
     switch_state_changed = 1;
-       
-      picAsong(1);led_update();
+   //
 }
 else if (!(p2val & SW2)){
-      switch_state_changed = 1; 
-      switch2_state_down = 1;
-    picAsong(2);
-  led_update();
+  switch_state_changed = 1; 
+  switch2_state_down = 1;
+  picAsong(2);
   switch2_state_down = 0;
 }
 else if (!(p2val & SW3)){
-      switch_state_changed = 1; 
-      switch3_state_down = 1;
-    picAsong(3);
-   led_update();
-   switch3_state_down = 0;;
+// 
+  switch3_state_down = 1;
+  picAsong(3);
+  switch3_state_down = 0;;
 }
 else if (!(p2val & SW4)){
-   switch_state_changed = 1;  
-    picAsong(4);
-    led_update();
+  switch_state_changed = 1;  
+  picAsong(4);
+//
 
 
 }
